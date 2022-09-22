17e04740f4b844844a41a7e6140a15063a79a1ac
Nika <nikarasevas@gmail.com>
2022-09-22 20:34:55 +0300

    Конструкция switch/ case/ default

---
+++ Example_1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Программа, которая по заданному номеру четверти показывает диапазон значений
+X и Y*/
+
+Console.WriteLine("Введи номер четверти");
+int number = int.Parse(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+    {
+        Console.WriteLine("Диапазон возможных значений четверти x > 0 и y > 0" );
+        break;
+    }
+    
+    case 2:
+    {
+        Console.WriteLine("Диапазон возможных значений четверти x < 0 и y > 0" );
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("Диапазон возможных значений четверти x < 0 и y < 0" );
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("Диапазон возможных значений четверти x > 0 и y < 0" );
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Такой четверти нет, смотри внимательнее!" );
+        break;
+    }
+}
